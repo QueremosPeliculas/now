QueremosPeliculas/now

commit abdb3f51d1f6d7f57e00ed0ef12bf4d557f98716

Allow overwriting custom certs with auto-renewable ones (#1082)
Currently it's not possible to create a new auto-renewable
certificate if a custom certificate already exist for a domain,
as we only allow the other direction from the cli.

Add a command flag that allows overwriting a custom certificate
with an auto-renewable certificate.

`now cert create --overwrite zeit.rocks`

src/providers/sh/util/certs.js
@@ -19,8 +19,8 @@ module.exports = class Certs extends Now {
     })
   }
 
-  create(cn) {
-    return this.createCert(cn)
+  create(cn, { overwrite }) {
+    return this.createCert(cn, { renew: true, overwriteCustom: overwrite })
   }
 
   renew(cn) {

src/providers/sh/commands/certs.js
@@ -48,6 +48,7 @@ const help = () => {
     -t ${chalk.bold.underline('TOKEN')}, --token=${chalk.bold.underline(
     'TOKEN'
   )}        Login token
+    --overwrite                    Overwrite existing custom certificate on create
     --crt ${chalk.bold.underline('FILE')}                     Certificate file
     --key ${chalk.bold.underline('FILE')}                     Certificate key file
     --ca ${chalk.bold.underline('FILE')}                      CA certificate chain file
@@ -62,6 +63,14 @@ const help = () => {
       ${chalk.cyan(
         '$ now certs replace --crt domain.crt --key domain.key --ca ca_chain.crt domain.com'
       )}
+
+  ${chalk.gray(
+    '–'
+  )} Create a new auto-renewable certificate replacing an existing custom certificate
+
+      ${chalk.cyan(
+        '$ now certs create --overwrite domain.com'
+      )}
 `)
 }
 
@@ -74,7 +83,7 @@ let subcommand
 const main = async ctx => {
   argv = mri(ctx.argv.slice(2), {
     string: ['crt', 'key', 'ca'],
-    boolean: ['help', 'debug'],
+    boolean: ['help', 'debug', 'overwrite'],
     alias: {
       help: 'h',
       debug: 'd'
@@ -196,6 +205,15 @@ async function run({ token, sh: { currentTeam, user } }) {
     let cert
 
     if (argv.crt || argv.key || argv.ca) {
+      if (argv.overwrite) {
+        console.error(error(
+          `Use replace command instead. Usage: ${chalk.cyan(
+            '`now certs replace --crt DOMAIN.CRT --key DOMAIN.KEY [--ca CA.CRT] <id | cn>`'
+          )}`
+        ))
+        return exit(1)
+      }
+
       // Issue a custom certificate
       if (!argv.crt || !argv.key) {
         console.error(error(
@@ -213,7 +231,7 @@ async function run({ token, sh: { currentTeam, user } }) {
       cert = await certs.put(cn, crt, key, ca)
     } else {
       // Issue a standard certificate
-      cert = await certs.create(cn)
+      cert = await certs.create(cn, { overwrite: argv.overwrite })
     }
     if (!cert) {
       // Cert is undefined and "Cert is already issued" has been printed to stdout

src/providers/sh/util/index.js
@@ -755,7 +755,7 @@ module.exports = class Now extends EventEmitter {
     })
   }
 
-  createCert(domain, { renew } = {}) {
+  createCert(domain, { renew, overwriteCustom } = {}) {
     return this.retry(
       async (bail, attempt) => {
         if (this._debug) {
@@ -766,7 +766,8 @@ module.exports = class Now extends EventEmitter {
           method: 'POST',
           body: {
             domains: [domain],
-            renew
+            renew,
+            overwriteCustom
           }
         })
 

commit 0d841d45fc7ece622e25374f3ab31dd5f84cd224

Fix creating a new default certificate
The commit abdb3f51d1f6d7f57e00ed0ef12bf4d557f98716 broke
creating new certificates with `now certs create`.

src/providers/sh/util/certs.js
@@ -20,7 +20,7 @@ module.exports = class Certs extends Now {
   }
 
   create(cn, { overwrite }) {
-    return this.createCert(cn, { renew: true, overwriteCustom: overwrite })
+    return this.createCert(cn, { renew: overwrite, overwriteCustom: overwrite })
   }
 
   renew(cn) {