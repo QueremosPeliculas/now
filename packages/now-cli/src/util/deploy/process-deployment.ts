QueremosPeliculas/now

commit 5844d9a6754cbdeeb7564e06508e2aecefdc182b

Fix event order in CLI (#3502)
This should fix [PRODUCT-893](https://zeit.atlassian.net/browse/PRODUCT-893) and [PRODUCT-941](https://zeit.atlassian.net/browse/PRODUCT-941)

packages/now-cli/src/util/deploy/process-deployment.ts
@@ -82,16 +82,6 @@ export default async function processDeployment({
           `Total files ${event.payload.total.size}, ${event.payload.missing.length} changed`
         );
 
-        if (!quiet) {
-          log(
-            `Synced ${pluralize(
-              'file',
-              event.payload.missing.length,
-              true
-            )} ${uploadStamp()}`
-          );
-        }
-
         const missingSize = event.payload.missing
           .map((sha: string) => event.payload.total.get(sha).data.length)
           .reduce((a: number, b: number) => a + b, 0);
@@ -121,6 +111,13 @@ export default async function processDeployment({
         now._host = event.payload.url;
 
         if (!quiet) {
+          log(
+            `Synced ${pluralize(
+              'file',
+              event.payload.missing.length,
+              true
+            )} ${uploadStamp()}`
+          );
           const version = isLegacy ? `${chalk.grey('[v1]')} ` : '';
           log(`https://${event.payload.url} ${version}${deployStamp()}`);
         } else {
@@ -128,7 +125,10 @@ export default async function processDeployment({
         }
 
         if (queuedSpinner === null) {
-          queuedSpinner = wait('Queued...');
+          queuedSpinner =
+            event.payload.readyState === 'QUEUED'
+              ? wait('Queued...')
+              : wait('Building...');
         }
       }
 
@@ -200,15 +200,6 @@ export default async function processDeployment({
         debug(
           `Total files ${event.payload.total.size}, ${event.payload.missing.length} changed`
         );
-        if (!quiet) {
-          log(
-            `Synced ${pluralize(
-              'file',
-              event.payload.missing.length,
-              true
-            )} ${uploadStamp()}`
-          );
-        }
 
         const missingSize = event.payload.missing
           .map((sha: string) => event.payload.total.get(sha).data.length)
@@ -239,6 +230,13 @@ export default async function processDeployment({
         now._host = event.payload.url;
 
         if (!quiet) {
+          log(
+            `Synced ${pluralize(
+              'file',
+              event.payload.missing.length,
+              true
+            )} ${uploadStamp()}`
+          );
           const version = isLegacy ? `${chalk.grey('[v1]')} ` : '';
           log(`${event.payload.url} ${version}${deployStamp()}`);
         } else {

packages/now-cli/src/commands/dev/dev.ts
@@ -17,9 +17,7 @@ export default async function dev(
   args: string[],
   output: Output
 ) {
-  output.dim(
-    `Now CLI ${pkg.version} dev (beta) — https://zeit.co/feedback`
-  );
+  output.dim(`Now CLI ${pkg.version} dev (beta) — https://zeit.co/feedback`);
 
   const [dir = '.'] = args;
   const cwd = path.resolve(dir);

packages/now-cli/src/commands/scale.js
@@ -18,7 +18,10 @@ import getMinFromArgs from '../util/scale/get-min-from-args';
 import patchDeploymentScale from '../util/scale/patch-deployment-scale';
 import waitVerifyDeploymentScale from '../util/scale/wait-verify-deployment-scale';
 import { handleError } from '../util/error';
-import { VerifyScaleTimeout, DeploymentTypeUnsupported } from '../util/errors-ts';
+import {
+  VerifyScaleTimeout,
+  DeploymentTypeUnsupported,
+} from '../util/errors-ts';
 import {
   DeploymentNotFound,
   DeploymentPermissionDenied,
@@ -28,7 +31,7 @@ import {
   InvalidMaxForScale,
   InvalidMinForScale,
   InvalidScaleMinMaxRelation,
-  NotSupportedMinScaleSlots
+  NotSupportedMinScaleSlots,
 } from '../util/errors-ts';
 import { InvalidAllForScale, InvalidRegionOrDCForScale } from '../util/errors';
 import handleCertError from '../util/certs/handle-cert-error';
@@ -56,7 +59,9 @@ const help = () => {
 
   ${chalk.dim('Examples:')}
 
-  ${chalk.gray('–')} Enable your deployment in all datacenters (min: 0, max: auto)
+  ${chalk.gray(
+    '–'
+  )} Enable your deployment in all datacenters (min: 0, max: auto)
 
     ${chalk.cyan('$ now scale my-deployment-123.now.sh all')}
 
@@ -87,7 +92,7 @@ export default async function main(ctx) {
     argv = getArgs(ctx.argv.slice(2), {
       '--verify-timeout': Number,
       '--no-verify': Boolean,
-      '-n': '--no-verify'
+      '-n': '--no-verify',
     });
   } catch (err) {
     handleError(err);
@@ -100,7 +105,10 @@ export default async function main(ctx) {
   }
 
   // Prepare the context
-  const { authConfig: { token }, config } = ctx;
+  const {
+    authConfig: { token },
+    config,
+  } = ctx;
   const { currentTeam } = config;
   const { apiUrl } = ctx;
   const debug = argv['--debug'];
@@ -155,8 +163,7 @@ export default async function main(ctx) {
   }
   if (dcs instanceof InvalidRegionOrDCForScale) {
     output.error(
-      `The value "${dcs.meta
-        .regionOrDC}" is not a valid region or DC identifier`
+      `The value "${dcs.meta.regionOrDC}" is not a valid region or DC identifier`
     );
     now.close();
     return 1;
@@ -165,8 +172,7 @@ export default async function main(ctx) {
   const min = getMinFromArgs(argv._);
   if (min instanceof InvalidMinForScale) {
     output.error(
-      `Invalid <min> parameter "${min.meta
-        .value}". A number or "auto" were expected`
+      `Invalid <min> parameter "${min.meta.value}". A number or "auto" were expected`
     );
     now.close();
     return 1;
@@ -175,24 +181,21 @@ export default async function main(ctx) {
   const max = getMaxFromArgs(argv._);
   if (max instanceof InvalidMinForScale) {
     output.error(
-      `Invalid <min> parameter "${max.meta
-        .value}". A number or "auto" were expected`
+      `Invalid <min> parameter "${max.meta.value}". A number or "auto" were expected`
     );
     now.close();
     return 1;
   }
   if (max instanceof InvalidArgsForMinMaxScale) {
     output.error(
-      `Invalid number of arguments: expected <min> ("${max.meta
-        .min}") and [max]`
+      `Invalid number of arguments: expected <min> ("${max.meta.min}") and [max]`
     );
     now.close();
     return 1;
   }
   if (max instanceof InvalidMaxForScale) {
     output.error(
-      `Invalid <max> parameter "${max.meta
-        .value}". A number or "auto" were expected`
+      `Invalid <max> parameter "${max.meta.value}". A number or "auto" were expected`
     );
     now.close();
     return 1;
@@ -262,14 +265,15 @@ export default async function main(ctx) {
     deployment.url
   );
   if (result instanceof ForbiddenScaleMinInstances) {
-    output.error(`You can't scale to more than ${result.meta.max} min instances with your current plan.`);
+    output.error(
+      `You can't scale to more than ${result.meta.max} min instances with your current plan.`
+    );
     now.close();
     return 1;
   }
   if (result instanceof ForbiddenScaleMaxInstances) {
     output.error(
-      `You can't scale to more than ${result.meta
-        .max} max instances with your current plan.`
+      `You can't scale to more than ${result.meta.max} max instances with your current plan.`
     );
     now.close();
     return 1;

packages/now-python/src/index.ts
@@ -36,11 +36,10 @@ export async function downloadFilesInWorkPath({
   files,
   meta,
   config,
-
 }: BuildOptions) {
-    debug('Downloading user files...');
-    let downloadedFiles = await download(files, workPath, meta);
-    if (meta && meta.isDev) {
+  debug('Downloading user files...');
+  let downloadedFiles = await download(files, workPath, meta);
+  if (meta && meta.isDev) {
     let base = null;
 
     if (config && config.zeroConfig) {

commit 0a34fdedb4033ba3f18630b314a6f4057a0183e4

Store file count on `file_count` event for future use (#3505)
Fixes an undefined length issue introduced by #3502

packages/now-cli/src/util/deploy/process-deployment.ts
@@ -59,6 +59,7 @@ export default async function processDeployment({
     let queuedSpinner = null;
     let buildSpinner = null;
     let deploySpinner = null;
+    let fileCount = null;
 
     for await (const event of createDeployment(
       nowClientOptions,
@@ -81,6 +82,7 @@ export default async function processDeployment({
         debug(
           `Total files ${event.payload.total.size}, ${event.payload.missing.length} changed`
         );
+        fileCount = event.payload.missing.length;
 
         const missingSize = event.payload.missing
           .map((sha: string) => event.payload.total.get(sha).data.length)
@@ -111,13 +113,7 @@ export default async function processDeployment({
         now._host = event.payload.url;
 
         if (!quiet) {
-          log(
-            `Synced ${pluralize(
-              'file',
-              event.payload.missing.length,
-              true
-            )} ${uploadStamp()}`
-          );
+          log(`Synced ${pluralize('file', fileCount, true)} ${uploadStamp()}`);
           const version = isLegacy ? `${chalk.grey('[v1]')} ` : '';
           log(`https://${event.payload.url} ${version}${deployStamp()}`);
         } else {
@@ -187,6 +183,8 @@ export default async function processDeployment({
       }
     }
   } else {
+    let fileCount = null;
+
     for await (const event of createLegacyDeployment(
       nowClientOptions,
       requestBody,
@@ -200,7 +198,7 @@ export default async function processDeployment({
         debug(
           `Total files ${event.payload.total.size}, ${event.payload.missing.length} changed`
         );
-
+        fileCount = event.payload.missing.length;
         const missingSize = event.payload.missing
           .map((sha: string) => event.payload.total.get(sha).data.length)
           .reduce((a: number, b: number) => a + b, 0);
@@ -230,13 +228,7 @@ export default async function processDeployment({
         now._host = event.payload.url;
 
         if (!quiet) {
-          log(
-            `Synced ${pluralize(
-              'file',
-              event.payload.missing.length,
-              true
-            )} ${uploadStamp()}`
-          );
+          log(`Synced ${pluralize('file', fileCount, true)} ${uploadStamp()}`);
           const version = isLegacy ? `${chalk.grey('[v1]')} ` : '';
           log(`${event.payload.url} ${version}${deployStamp()}`);
         } else {